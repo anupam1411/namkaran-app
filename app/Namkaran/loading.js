178bb2aa2add485c1c52bb1ec3e03886183aed0d
anupam1411 <sagaranupam2@gmail.com>
2024-12-05 14:08:47 +0530

    updates

---
+++ app/Namkaran/loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-2xl font-semibold">Loading Namkaran Form...</h1>
+    </div>
+  );
+}
